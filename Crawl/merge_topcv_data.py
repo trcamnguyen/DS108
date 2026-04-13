462c3b6550a1ee658872b8367cce3e5156d81628
Tran Ngoc Cam Nguyen <camnguyenz218@gmail.com>
2026-04-13 19:56:00 +0700

    merge data

---
+++ Crawl/merge_topcv_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+
+def merge_csv_files():
+    data_dir = r"C:\Users\tncn2\Documents\HK2-2026_2027\DS108\DS108\Crawl\data"
+    
+    file1_path = os.path.join(data_dir, "topcv_person1.csv")
+    file2_path = os.path.join(data_dir, "topcv_person2.csv")
+    out_path = os.path.join(data_dir, "topcv_merged.csv")
+
+    print(f"Reading file: {file1_path}")
+    df1 = pd.read_csv(file1_path)
+    print(f"Number of records in file 1: {df1.shape[0]}")
+
+    print(f"Reading file: {file2_path}")
+    df2 = pd.read_csv(file2_path)
+    print(f"Number of records in file 2: {df2.shape[0]}")
+
+    merged_df = pd.concat([df1, df2], ignore_index=True)
+    print(f"Total records after merging: {merged_df.shape[0]}")
+
+    # Remove duplicated rows based on url
+    merged_df.drop_duplicates(subset=['url'], keep='first', inplace=True)
+    print(f"Total records after removing duplicates: {merged_df.shape[0]}")
+
+    # Save to out_path
+    merged_df.to_csv(out_path, index=False, encoding='utf-8-sig') 
+    print(f"\n=> Successfully saved merged data to: {out_path}")
+
+if __name__ == "__main__":
+    merge_csv_files()
